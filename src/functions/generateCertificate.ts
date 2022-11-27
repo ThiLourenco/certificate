2f92bf0923a02e71fcd22465f6fa1bb21e46738c
Thigo <thiago.l17@yahoo.com>
2022-11-27 19:38:19 -0300

    refactor(): adding cerfiticate in S3

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -31,16 +31,6 @@ const compileTemplate = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document.put({
-    TableName: 'users_certificate',
-    Item: {
-      id,
-      name,
-      grade,
-      created_at: new Date().getTime(),
-    }
-  }).promise();
-
   const response = await document.query({
     TableName: 'users_certificate',
     KeyConditionExpression: 'id = :id',
@@ -48,6 +38,20 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ':id' : id
     }
   }).promise();
+
+  const userAlreadyExists = response.Items[0];
+  
+  if (userAlreadyExists) {
+    await document.put({
+      TableName: 'users_certificate',
+      Item: {
+        id,
+        name,
+        grade,
+        created_at: new Date().getTime(),
+      }
+    }).promise();
+  }
 
   const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png');
   const medal = readFileSync(medalPath, 'base64')
